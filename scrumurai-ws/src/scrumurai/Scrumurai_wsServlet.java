1de8bbfe36eb9b44e812dc3cb55decbf2beb71bf
vision810 <vision810@gmail.com>
2013-04-26 10:53:08 +0200

    create method for user

--- scrumurai-ws/src/scrumurai/Scrumurai_wsServlet.java
+++ scrumurai-ws/src/scrumurai/Scrumurai_wsServlet.java
@@ -2,12 +2,10 @@ package scrumurai;
 
 import java.io.IOException;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
 import javax.servlet.http.*;
 
-import scrumurai.data.EMF;
 import scrumurai.data.entities.User;
+import scrumurai.data.mapping.DataMapper;
 
 @SuppressWarnings("serial")
 public class Scrumurai_wsServlet extends HttpServlet {
@@ -15,21 +13,13 @@ public class Scrumurai_wsServlet extends HttpServlet {
 			throws IOException {
 		resp.setContentType("text/plain");
 		System.out.println("AAAAAAAAAAA");
-		EntityManager em = EMF.get().createEntityManager();
-		try {
-			User u = new User();
-			u.setEmail("dejan.svetec0@gmail.com");
-			u.setFirstName("Dejan");
-			u.setLastName("Svetec");
-			u.setPassword("pass");
-			u.setUsername("dejan");
-			em.getTransaction().begin();
-			em.persist(u);
-			em.getTransaction().commit();
-			Query q1 = em.createQuery("SELECT c FROM User c");
-			System.out.println(q1.getResultList().size());
-			resp.getWriter().println("Hello, world");
-		} finally {
-		}
+		User u = new User();
+		u.setEmail("dejan.svetec0@gmail.com");
+		u.setFirstName("Dejanaaaa");
+		u.setLastName("Svetec");
+		u.setPassword("pass");
+		u.setUsername("dejan");
+		DataMapper.create(u);
+		System.out.println("DONE");
 	}
 }
---
+++ scrumurai-ws/src/scrumurai/data/mapping/DataMapper.java
@@ -0,0 +1,20 @@
+package scrumurai.data.mapping;
+
+import javax.persistence.EntityManager;
+
+import scrumurai.data.EMF;
+import scrumurai.data.entities.User;
+
+public class DataMapper {
+	
+	private static EntityManager em;
+	
+	public static void create(User u) {
+		em = EMF.get().createEntityManager();
+		em.getTransaction().begin();
+		em.persist(u);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+}
