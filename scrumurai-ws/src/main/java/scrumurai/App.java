4b00330775629012fd8760bfe5f59b4bb72d77d0
Dejan Svetec <dejan.svetec0@gmail.com>
2013-04-29 12:36:24 +0200

    durable database persistence.xml

--- scrumurai-ws/src/main/java/scrumurai/App.java
+++ scrumurai-ws/src/main/java/scrumurai/App.java
@@ -1,18 +1,8 @@
 package scrumurai;
 
 import java.util.List;
-import java.util.Properties;
-
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 import scrumurai.data.entities.User;
 import scrumurai.data.mapping.DataMapper;
-import scrumurai.model.UserDetails;
 
 /**
  * Hello world!
