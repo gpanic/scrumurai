8688d80f0eb0f64fa6069a0f65dd9cb11e260b2d
vision810 <vision810@gmail.com>
2013-06-04 11:01:14 +0200

    drop db method

--- scrumurai-ws/src/main/java/scrumurai/ws/resources/DBResource.java
+++ scrumurai-ws/src/main/java/scrumurai/ws/resources/DBResource.java
@@ -1,10 +1,13 @@
 package scrumurai.ws.resources;
 
 import java.io.File;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import scrumurai.data.EMF;
 
 @Path("/db")
 public class DBResource {
@@ -13,8 +16,24 @@ public class DBResource {
     @Path("/drop")
     @Produces(MediaType.TEXT_PLAIN)
     public String drop() {
-        File f = new File("scrumurai.sqlite");
-        f.delete();
+        EntityManager em = EMF.get().createEntityManager();
+        em.getTransaction().begin();
+        Query query;
+        query = em.createQuery("DELETE FROM ProjectMember");
+        query.executeUpdate();
+        query = em.createQuery("DELETE FROM UserStory");
+        query.executeUpdate();
+        query = em.createQuery("DELETE FROM Sprint");
+        query.executeUpdate();
+        query = em.createQuery("DELETE FROM Release");
+        query.executeUpdate();
+        query = em.createQuery("DELETE FROM Project");
+        query.executeUpdate();
+        query = em.createQuery("DELETE FROM User");
+        query.executeUpdate();
+        query = em.createQuery("DELETE FROM UserEffortVote");
+        query.executeUpdate();
+        em.getTransaction().commit();
         return "DROPPED";
     }
     
